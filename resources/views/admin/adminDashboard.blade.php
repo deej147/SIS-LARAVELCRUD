7f74fbd5a6d853f98df18dbeebcef92d2dc690e5
Vincent <2001105940@student.buksu.edu.ph>
2025-02-23 12:02:13 +0800

    pa chuy chuy sa admin dashboard

--- resources/views/admin/adminDashboard.blade.php
+++ resources/views/admin/adminDashboard.blade.php
@@ -10,7 +10,7 @@
     <!-- Content Row -->
     <div class="row">
         <!-- Users Card -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 mb-4 mx-auto">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -28,7 +28,7 @@
         </div>
 
         <!-- Admin Card -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 mb-4 mx-auto">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -46,7 +46,7 @@
         </div>
 
         <!-- Regular Users Card -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-3 col-md-6 mb-4 mx-auto">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
